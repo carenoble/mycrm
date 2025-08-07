f8f97f7e940689171507e3543759edc8a14f4f9c
CareNoble <admin@carenoble.com>
2025-08-07 16:38:36 +0300

    Fix jsonwebtoken import syntax to resolve TypeScript declaration error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient()
 
